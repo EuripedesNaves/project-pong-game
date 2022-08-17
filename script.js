75c3ef44e42e6947124c02e78b851fa78baa6c7e
Eurípedes <euripedes.naves@outlook.com>
2022-08-16 22:37:11 -0300

    Body height ajusted

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-
-
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
+const width = 600;
+const height = 400;
 let xBolinha = 300;
 let yBolinha = 200;
 let diametro = 15;
@@ -13,9 +13,9 @@ let largura1 = 10;
 let altura1 = 90;
 let xRaquete2 = 585;
 let yRaquete2 = 150;
-//let velocidadeYoponente;
 let xVelocidade = 6;
 let yVelocidade = 6;
+let colidiu = false;
 
 function drawElements() {
 
@@ -28,13 +28,12 @@ function drawElements() {
   ctx.closePath();
 
 
-  //Variáveis da Raquete1
-  
-  //let yRaquete1 = 150;
+  //Criação da Raquete1
+
   ctx.fillStyle = 'white';
   ctx.fillRect(xRaquete1, yRaquete1, largura1, altura1);
 
-  //Variáveis da Raquete2
+  //Criação da Raquete2
   ctx.fillStyle = 'white';
   ctx.fillRect(xRaquete2, yRaquete2, largura1, altura1);
 
@@ -52,8 +51,18 @@ function drawElements() {
 
 }
 
+function verificaColisão() {
+  if (xBolinha + diametro > width || xBolinha - diametro < 0) {
+    xVelocidade *= -1;
+  }
+
+  if (yBolinha + diametro > height || yBolinha - diametro < 0) {
+    yVelocidade *= -1;
+  }
+}
+
 function movimentaRaquete2() {
-  yRaquete2 = yBolinha - altura1 /2 - 30;
+  yRaquete2 = yBolinha - altura1 / 2 - 30;
 }
 
 function movimentaBolinha() {
@@ -61,12 +70,14 @@ function movimentaBolinha() {
   yBolinha += yVelocidade;
 }
 
+
 function upDateGame() {
   ctx.clearRect(0, 0, 600, 400);
   drawElements();
   movimentaBolinha();
   movimentaRaquete2();
-  requestAnimationFrame(upDateGame)
+  verificaColisão();
+  requestAnimationFrame(upDateGame);
 }
 
 document.addEventListener('keydown', (event) => {
@@ -80,8 +91,5 @@ document.addEventListener('keydown', (event) => {
   }
 
 })
-
-
-
 
 upDateGame();
