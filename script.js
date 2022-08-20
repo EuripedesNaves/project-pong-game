52126c9beaef6615cfb6d6c3791c2bd2e05849dc
Eurípedes <euripedes.naves@outlook.com>
2022-08-20 00:23:33 -0300

    Botão de Start

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ const ctx = canvas.getContext('2d');
 const width = 600;
 const height = 400;
 
+
 // Dados bolinha 
 let xBolinha = 300;
 let yBolinha = 200;
 let raio = 15;
+
 
 // Dados Raquete Esquerda (1)
 let xRaquete1 = 5;
@@ -24,8 +26,8 @@ let largura = 10;
 let altura = 90;
 
 //Velocidade do jogo
-let xVelocidade = 7;
-let yVelocidade = 7;
+let xVelocidade = 3;
+let yVelocidade = 0;
 
 //Placar Jogador
 let xPlacarEsquerda = 150;
@@ -67,7 +69,7 @@ function drawElements() {
   ctx.strokeRect(xPlacarEsquerda, yPlacar, widthPlacar, heigthPlacar);
   ctx.fillStyle = 'white';
   ctx.font = '15px Calibri';
-  ctx.fillText(meusPontos, 166, yPlacar/2+heigthPlacar);
+  ctx.fillText(meusPontos, 166, yPlacar / 2 + heigthPlacar);
 
   //Placar 2
   ctx.fillStyle = 'orange';
@@ -76,10 +78,10 @@ function drawElements() {
   ctx.strokeRect(xPlacarDireita, yPlacar, widthPlacar, heigthPlacar);
   ctx.fillStyle = 'white';
   ctx.font = '15px Calibri';
-  ctx.fillText(pontosOponente, 467, yPlacar/2+heigthPlacar);
+  ctx.fillText(pontosOponente, 467, yPlacar / 2 + heigthPlacar);
 }
 
-//Chuva de funções
+//Função para verificar colisão com o campo de jogo
 function verificaColisão() {
   if (xBolinha + raio > width || xBolinha - raio < 0) {
     xVelocidade *= -1;
@@ -88,57 +90,52 @@ function verificaColisão() {
     yVelocidade *= -1;
   }
 }
-
+//Função para movimentar a raquete2
 function movimentaRaquete2() {
- yRaquete2 = yBolinha - altura/8 - 25;
+  yRaquete2 = yBolinha - altura / 8 - 25;
 }
 
+//Função para movimentar a bolinha
 function movimentaBolinha() {
   xBolinha += xVelocidade;
   yBolinha += yVelocidade;
 }
 
+//Função para identificar colisão da bolinha com a raquete
 function verificaColisãoRaquete() {
-if(xBolinha + raio > xRaquete2 + largura/2 &&
-  yBolinha - raio < yRaquete2 + altura &&
-  yBolinha + raio > yRaquete2) {
-  xVelocidade *= -1;
-}
-if(xBolinha + raio < xRaquete1 + largura &&
-   yBolinha - raio < yRaquete1 + altura &&
-   yBolinha + raio < yRaquete1) {
-  xVelocidade *= -1;
-}
+  if (xBolinha + raio > xRaquete2 + largura / 2 &&
+    yBolinha - raio < yRaquete2 + altura &&
+    yBolinha + raio > yRaquete2) {
+    xVelocidade *= -1;
+  }
+  if (xBolinha + raio < xRaquete1 + largura &&
+    yBolinha + raio < yRaquete1 + altura &&
+    yBolinha + raio < yRaquete1) {
+    xVelocidade *= -1;
+  }
 }
 
 //Marcador de Pontos
-function pontos(){
-  if(xBolinha > 585){
+function pontos() {
+  if (xBolinha > 585) {
     meusPontos += 1;
   }
-  if(xBolinha < 15){
+  if (xBolinha < 15) {
     pontosOponente += 1;
-  } 
+  }
 }
 
 function gameOver() {
-  if(pontosOponente > 1) {
+  if (pontosOponente > 15) {
     ctx.clearRect(0, 0, 600, 400);
 
     let img = new Image()
     img.src = 'GameOver.jpg'
 
-    ctx.drawImage(img, 100, 100, 400, 200);
-
-
+    ctx.drawImage(img, 100, 90, 400, 200);
   }
 }
 
-function reset() {
-  ctx.clearRect(0, 0, 600, 400);
-  
-
-}
 
 //Função para animação do Game
 function upDateGame() {
@@ -154,7 +151,6 @@ function upDateGame() {
 }
 
 
-
 //Insrução para reconhecimento do Teclado
 document.addEventListener('keydown', (event) => {
   const key = event.code
@@ -166,4 +162,9 @@ document.addEventListener('keydown', (event) => {
   }
 })
 
-upDateGame();
+// -------------------------------------------------------------------------
+
+
+
+
+
